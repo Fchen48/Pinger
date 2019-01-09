7eb5a0f79014705fb9ddeeb748270dac4417c3d7
Fchen <mario@fridrichovsky.at>
2019-01-09 19:20:42 +0100

    removed unused modules removed unused code added workaround for electron maximizeable bug shortened errorhandling activated tray icon update

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const url = require("url");
 const path = require("path");
 const ping = require("./functions/ping");
 const log = require("./functions/log");
-const render = require("puppeteer-render-text");
 const jimp = require("jimp");
 const AutoLaunch = require("auto-launch");
 
@@ -18,31 +17,11 @@ const errorlog = path.join(electron.app.getPath("userData"), "logs", "error.log"
 const lastResults = [];
 const autostart = new AutoLaunch({ name: "Pinger" });
 
-// const generateTrayIcon = text => new Promise((resolve, reject) => {
-//     render({
-//         text,
-//         output: "tray.png",
-//         width: 64,
-//         height: 64,
-//         style: {
-//             fontFamily: "sans-serif",
-//             fontSize: 56,
-//             color: "white",
-//             width: "100%",
-//             height: "100%",
-//             "line-height": "64px",
-//             "text-align": "center",
-//             margin: 0
-//         }
-//     })
-//     .then(() => resolve(path.join(__dirname, "tray.png")))
-//     .catch(error => reject(error));
-// });
 
 const generateTrayIcon = text => new Promise((resolve, reject) => {
     jimp.loadFont(jimp.FONT_SANS_64_WHITE)
     .then(font => {
-        jimp.read(80, 80, 0xfff)
+        jimp.read(80, 80)
         .then(img => {
             img.print(font, 0, 0, {
                 text,
@@ -50,7 +29,6 @@ const generateTrayIcon = text => new Promise((resolve, reject) => {
                 alignmentY: jimp.VERTICAL_ALIGN_MIDDLE
             }, 80, 80)
             .resize(64, 64)
-            .write("test.png")
             .getBuffer(jimp.MIME_PNG, (error, buffer) => {
                 trayIcon = electron.nativeImage.createFromBuffer(buffer);
                 return resolve();
@@ -76,37 +54,49 @@ electron.app.on("ready", () => {
             height,
             minHeight: height,
             maxHeight: height,
-            resizeable: false,
             icon: path.join(__dirname, "images", "icon.png")
         };
+
         Object.assign(woptions, config.get("window"));
         mainWindow = new electron.BrowserWindow(woptions);
+
         mainWindow.loadURL(url.format({
             pathname: path.join(__dirname, "html", "index.html"),
             protocol: "file:",
             slashes: true
         }));
+
         mainWindow.on("close", () => {
             config.set("window", mainWindow.getBounds());
         });
+
         mainWindow.on("closed", () => {
             electron.app.quit();
         });
+
         mainWindow.on("hide", () => {
-            // tray = new electron.Tray(path.join(__dirname, "tray.png"));
-            console.log(trayIcon);
             tray = new electron.Tray(trayIcon);
             tray.on("click", () => {
                 mainWindow.show();
-
             });
         });
+
         mainWindow.on("show", () => {
             tray.destroy();
         });
+
         mainWindow.once("ready-to-show", () => {
             mainWindow.show();
         });
+
+        mainWindow.on("maximize", () => {
+            console.log("full-screen");
+            return;
+        });
+
+        mainWindow.setFullScreenable(false);
+        mainWindow.setMaximizable(false);
+        mainWindow.setResizable(false);
     }, 100);
 });
 
@@ -132,7 +122,7 @@ electron.ipcMain.on("window:autostart", () => {
         config.set("autostart", true);
         mainWindow.webContents.send("autostart");
     })
-    .catch(error => console.error(error));
+    .catch(console.error);
 });
 
 electron.ipcMain.on("startPing", (event, data) => {
@@ -173,13 +163,10 @@ ping.on("ping", result => {
     if(tray) {
         if(!tray.isDestroyed()) {
             generateTrayIcon(valueIcon)
-            // .then(() => {
-            //     if(!tray.isDestroyed()) {
-
-            //         tray.setImage(path.join(__dirname, "tray.png"));
-            //     }
-            // })
-            .catch(error => console.error(error));
+            .then(() => {
+                if(!tray.isDestroyed()) tray.setImage(trayIcon);
+            })
+            .catch(console.error);
         }
     }
 });
